5a287b7bd88d194e9b59d492945426323cd8d8a3
Michelle Balcazar <michelle.balcazar.ortiz@gmail.com>
2026-07-30 17:59:35 -0600

    Migrate login logic to Angular

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  username = '';
+  password = '';
+  botonHabilitado = false;
 
+  mensaje = '';
+  tipoMensaje = '';
+
+  usuarios = [
+  {
+    nombre: 'admin',
+    password: '123456'
+  },
+  {
+    nombre: 'michelle',
+    password: 'qwerty'
+  }
+];
+
+  validarFormulario() {
+    this.botonHabilitado =
+    this.username.trim().length >= 3 &&
+    this.password.trim().length >= 6;
+}
+
+iniciarSesion() {
+  const user = this.username.trim();
+  const password = this.password.trim();
+
+  const accesoPermitido = this.validarCredenciales(user, password);
+
+  if (accesoPermitido) {
+    this.mensaje = 'Inicio de sesión exitoso';
+    this.tipoMensaje = 'success';
+  } else {
+    this.mensaje = 'Usuario o contraseña incorrectos';
+    this.tipoMensaje = 'error';
+  }
+}
+
+validarCredenciales(user: string, password: string): boolean {
+  const usuarioEncontrado = this.usuarios.find(function(usuario) {
+    return usuario.nombre === user;
+  });
+
+  if (usuarioEncontrado && usuarioEncontrado.password === password) {
+    return true;
+  }
+
+  return false;
+}
 }
